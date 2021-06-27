47b1a83aa507b6e8ba002eed1e444c1b58e91afb
Arley David Velasco <arley2180984@correo.uis.edu.co>
2021-06-27 15:05:50 -0500

    main first commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+/* global test */
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+  /* global expect */
   expect(linkElement).toBeInTheDocument();
 });
